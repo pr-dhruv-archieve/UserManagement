9e3f0add5f11492d6dad8af3f13f2da45e132649
Mahendra Prajapati <pra_mahendra@hotmail.com>
2020-06-10 04:06:34 +0530

    RestControllerEndPoint is added

---
+++ src/main/java/com/soma/UserManagement/actuatorendpoints/MyRestControllerEndpoint.java
@@ -0,0 +1,16 @@
+package com.soma.UserManagement.actuatorendpoints;
+
+import org.springframework.boot.actuate.endpoint.web.annotation.RestControllerEndpoint;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RestControllerEndpoint(id = "restEndpoint")
+public class MyRestControllerEndpoint {
+    @GetMapping
+    public ResponseEntity<String> getResponse() {
+        return new ResponseEntity<>("RestController Response endpoint", HttpStatus.OK);
+    }
+}
